e102153488c4e47df3f9cd8249498b93469c5d86
Taj <tta16@my.fsu.edu>
2019-03-25 12:54:58 -0400

    Graph gets saved as png and predictor model has made progress, but is not corrected

--- sourcecode/predictor.py
+++ sourcecode/predictor.py
@@ -3,7 +3,7 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model,model_selection
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, Normalizer
 import datetime
 
 def predictor(ticker,infofile,graphfile,col,t):
@@ -25,22 +25,36 @@ def predictor(ticker,infofile,graphfile,col,t):
             col_data.append(row[col])
 
     #sets up linear regression model
-    #time_data_encoded = LabelEncoder().fit_transform(time_data)
-    #model = linear_model.LinearRegression()
-    #model.fit(time_data_encoded,col_data)
-    #model.fit(time_data,col_data)
 
-    #goes through prediction loop 
+    model = linear_model.LinearRegression()
+
+    time_data_encoded = LabelEncoder().fit_transform(time_data)
+    col_data_encoded = LabelEncoder().fit_transform(col_data)
+
+    time_data_encoded = np.array(time_data_encoded).reshape(-1,1)
+    col_data_encoded = np.array(col_data_encoded).reshape(-1,1)
+
+    model.fit(time_data_encoded,col_data_encoded)
+
+    #finds times to evaluate
     for i in range(t):
-        currentDT = start_time.replace(minute=start_time.minute+(i+1))
+        currentDT = start_time + datetime.timedelta(seconds=60*(i+1))
         test_time.append(currentDT.strftime('%H:%M'))
-        #predictions.append(model.predict(test_time))
-        predictions.append(col_data[0])
+
+    #goes through prediction loop
+    test_time_encoded = LabelEncoder().fit_transform(test_time)
+    test_time_encoded = np.array(test_time_encoded).reshape(-1,1)
+    predictions = model.predict(test_time_encoded)
+    #print(test_time)
+    #print(test_time_encoded)
+    #for outer in test_time_encoded:
+    #    for time in test_time_encoded(outer):
+    #        predictions.append(model.predict(time))
 
     #output graph with historical data and predicted data. Color seperated
-    show_graph(time_data,col_data,test_time,predictions)
+    save_graph(time_data,col_data,test_time,predictions)
 
-def show_graph(time,results,pred_time,pred_results):
+def save_graph(time,results,pred_time,pred_results):
     """
     Displays graph based on time and col historical data as well as the
     predicted results from the linear regression model
@@ -50,7 +64,12 @@ def show_graph(time,results,pred_time,pred_results):
     graph = plt.figure()
     ax = graph.add_subplot(1,1,1)
     ax.plot(time,results,'r',pred_time,pred_results,'b')
-    plt.show()
+    plt.xlabel("Time")
+    plt.ylabel(sys.argv[4])
+
+    #saves graph
+    plt.savefig(sys.argv[3]+".png")
+
 
 if __name__ == "__main__":
     predictor(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],int(sys.argv[5]))
